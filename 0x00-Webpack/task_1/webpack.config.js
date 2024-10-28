3074d60afcf8ea2c14a0923e5b65479d6a476788
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-10-28 16:56:59 +0100

    task 2 -

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,18 +1,12 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        main: path.resolve(__dirname, './js/dashboard_main.js'),
-      },
-      output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
-      },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack Output",
-    }),
-  ],
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
 };
