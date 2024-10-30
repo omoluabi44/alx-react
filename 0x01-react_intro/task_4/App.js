4b4cd12999b8f2e445c4fdabcc8b500fb44bc29e
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-10-30 18:37:11 +0100

    task 8

---
+++ 0x01-react_intro/task_4/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import logo from "./holberton-logo.jpg"
+import {getFullYear, getFooterCopy} from './utils';
+
+
+function App() {
+  return (
+    <div className='App'>
+    <div className="App-header" >
+      <img src={logo} alt="holberton-logo" />
+      <h1>School dashboard </h1>
+    </div>
+
+    <div className='App-body'>Login to access the full dashboard </div>
+    <form>
+    <label htmlFor='email'>Email</label>
+    <input type="email" id='email' />
+    <label htmlFor='password'>password</label>
+    <input type="password" id='password'/>
+    <button>Ok</button>
+    </form>
+
+    <div className='App-footer'> Copyright {getFooterCopy()} - {getFullYear()}</div>
+    </div>
+  );
+}
+
+export default App;
