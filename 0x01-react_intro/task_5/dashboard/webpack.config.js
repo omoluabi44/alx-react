e68ef2f8ce5391a3033716ea49c14dba028527bc
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-10-30 19:43:26 +0100

    task

--- 0x01-react_intro/task_5/dashboard/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -1,49 +1,54 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development', // Use 'production' for production builds
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        clean: true, // Clean the output directory before emit
-    },
-    devServer: {
-        static: './dist', // Serve files from the dist folder
-        hot: true, // Enable hot module replacement
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/, // Transform ES6+ using Babel
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                    },
-                },
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        // type: 'asset/resource',
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
             },
-            {
-                test: /\.css$/, // Load CSS files
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/i, // Load images
-                type: 'asset/resource',
-            },
-            {
-                test: /\.jsx?$/, // Load files for source maps
-                use: 'source-map-loader',
-                enforce: 'pre',
-            },
+          },
         ],
-    },
-    devtool: 'inline-source-map', // Enable source maps
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './dist/index.html', // Use your HTML file as a template
-        }),
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
     ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    static: "./dist",
+    compress: true,
+    open: true,
+    hot: true,
+    port: 8564,
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      name: "index.html",
+      inject: false,
+      template: "./dist/index.html",
+    }),
+  ],
 };
