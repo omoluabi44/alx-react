c09c58983adc470d0c3933ca95b262c5d0b80345
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-10-30 19:40:09 +0100

    still on

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.css'; // Sample CSS file
-
-const App = () => {
-    return <h1>Hello, React with Webpack!</h1>;
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
