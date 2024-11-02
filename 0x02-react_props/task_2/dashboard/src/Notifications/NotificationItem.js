3d2c3ff655ae321834944a7fd946c006d7367250
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-11-02 14:41:02 +0100

    task 3

--- 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -9,3 +9,4 @@ const NotificationItem = ({ type, html, value }) => {
 }
 
 export default NotificationItem;
+
--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Notifications.css'
 import closeIcon from '../assets/close-icon.png'
 import { getLatestNotification } from '../utils/utils';
-import NotificationItem from './NotificationItem';
+import NotificationItem from "./NotificationItem";
 
  
 const Notifications = () => {
@@ -12,15 +12,15 @@ const Notifications = () => {
        <button
         style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontSize: "15px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
         aria-label="Close"
-        onClick={()=>console.log("Close button has been clicked")}
+        onClick={console.log("Close button has been clicked")}
       >
         <img src={closeIcon} alt="closeIcon" width="10px" />
       </button>
          <p>Here is the list of notifications </p>
          <ul>
-                <NotificationItem type="default" value="New course available" html={{ __html: '' }}  />
-                <NotificationItem  type="urgent" value="New resume available" html={{ __html: '' }}  />
-                <NotificationItem type="urgent" value="" html={ getLatestNotification()}/>
+         <NotificationItem type="default" value="New course available" />
+        <NotificationItem type="urgent" value="New resume available" />
+        <NotificationItem type="urgent" html={getLatestNotification()} />
             </ul>
         </div>
 
