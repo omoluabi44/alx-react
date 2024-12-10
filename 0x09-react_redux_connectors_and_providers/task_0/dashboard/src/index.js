8d8e5a51e4f46ac97e735aebe46b88f2397fca1a
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-12-10 17:13:34 +0100

    task2

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux"
+import uiReducer from "./reducers/uiReducer";
 
+
+const store = createStore(uiReducer)
 ReactDOM.render(
   <React.StrictMode>
+    <Provider>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
